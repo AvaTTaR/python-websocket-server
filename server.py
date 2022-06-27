4abb622413afe32cb1ebfde281a7fc788585d985
Ihar <avattargrey@gmail.com>
2022-06-27 03:02:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,23 +3,24 @@ from websocket_server import WebsocketServer
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	server.send_message_to_all("Hey all, a new client has joined us on Websocket-Server-1")
 
 
 # Called for every client disconnecting
 def client_left(client, server):
 	print("Client(%d) disconnected" % client['id'])
-
+	server.send_message_to_all("Websocket-Server-1 - Client(%d) disconnected: " % client['id'])
 
 # Called when a client sends a message
 def message_received(client, server, message):
 	if len(message) > 200:
 		message = message[:200]+'..'
 	print("Client(%d) said: %s" % (client['id'], message))
+	server.send_message_to_all("Websocket-Server-1 - client sad: "  % (client['id'], message))
 
 
 PORT=9001
-server = WebsocketServer(port = PORT)
+server = WebsocketServer(host='0.0.0.0', port=9001)
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
